778a24baf7d9cc44fe771d069afbebca8fe131c5
cooperandy2706-sketch <cooperandy2706@gmail.com>
2026-04-26 02:38:54 +0000

    redirect

--- next.config.js
+++ next.config.js
@@ -26,11 +26,6 @@ const nextConfig = {
       ],
     }]
   },
-  async redirects() {
-    return [
-      { source: '/', destination: '/feed', permanent: false },
-    ]
-  },
 }
 
 module.exports = nextConfig
